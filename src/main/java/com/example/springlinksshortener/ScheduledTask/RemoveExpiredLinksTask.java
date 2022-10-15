c340f3e108f058e2835f400e6387d7f61df1925b
Vladislav Knyshov <inova136@gmail.com>
2022-10-15 20:44:23 +0300

    feat: add RemoveExpiredLinksTask scheduled task

---
+++ src/main/java/com/example/springlinksshortener/ScheduledTask/RemoveExpiredLinksTask.java
@@ -0,0 +1,28 @@
+package com.example.springlinksshortener.ScheduledTask;
+
+import com.example.springlinksshortener.service.LinkService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RemoveExpiredLinksTask {
+
+    private LinkService linkService;
+    private static final Logger log = LoggerFactory.getLogger(RemoveExpiredLinksTask.class);
+
+    @Autowired
+    public RemoveExpiredLinksTask(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @Scheduled(fixedRate = 600000)
+    public void removeExpiredLinks() {
+        log.info("Start RemoveExpiredLinksTask");
+        int linksDeleted = linkService.deleteExpiredLinks();
+        log.info("Finish RemoveExpiredLinksTask; Deleted " + linksDeleted + " links.");
+    }
+}
