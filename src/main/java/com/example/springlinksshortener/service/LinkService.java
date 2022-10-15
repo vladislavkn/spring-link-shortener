ad2a6705351647e21a2918861a316e04671e1edd
Vladislav Knyshov <inova136@gmail.com>
2022-10-15 20:44:09 +0300

    feat: add deleteExpiredLinks method

--- src/main/java/com/example/springlinksshortener/service/LinkService.java
+++ src/main/java/com/example/springlinksshortener/service/LinkService.java
@@ -9,6 +9,8 @@ import com.example.springlinksshortener.repository.LinkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Service
 public class LinkService {
     private LinkRepository repository;
@@ -38,5 +40,17 @@ public class LinkService {
         Link link = repository.findByHash(hash);
         if (link == null) throw new LinkNotFoundException(hash);
         repository.deleteById(link.getId());
+    }
+
+    public int deleteExpiredLinks() {
+        AtomicInteger linksDeleted = new AtomicInteger();
+        repository.findAll().stream().forEach(link -> {
+            if(link.isExpired()) {
+                repository.deleteById(link.getId());
+                linksDeleted.addAndGet(1);
+            }
+        });
+
+        return linksDeleted.get();
     }
 }
