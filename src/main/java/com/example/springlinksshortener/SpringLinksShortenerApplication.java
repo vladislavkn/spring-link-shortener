b89f907a3d2fb4e514dde98e445cf95f2202a113
Vladislav Knyshov <inova136@gmail.com>
2022-10-15 18:25:58 +0300

    initial commit

---
+++ src/main/java/com/example/springlinksshortener/SpringLinksShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlinksshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLinksShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLinksShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springlinksshortener/SpringLinksShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlinksshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLinksShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
